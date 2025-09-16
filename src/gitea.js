3d9afce6f9431a6798983c1d3c8e5773e63d0539
Tiago Terenas Almeida <tiagoterenas@gmail.com>
2025-09-16 14:08:45 +0100

    Add Explicit Error

--- src/gitea.js
+++ src/gitea.js
@@ -37,7 +37,7 @@ async function api(method, url, data) {
     },
     body: JSON.stringify(data),
   });
-  if (!response.ok) throw new Error(response.statusText);
+  if (!response.ok) throw new Error(`API error: ${JSON.stringify(response)}`);
   return await response.json();
 }
 
